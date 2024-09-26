2937f3d7af979eb3ab246e517b950c7009141f41
Karla Lazo <karlalazoot@gmail.com>
2024-09-26 15:59:29 -0500

    se agregó borde a las figuras

--- sketch.js
+++ sketch.js
@@ -1,14 +1,20 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(4);
+  frameRate(50);
   background(0);
   rectMode(CENTER);
 }
 
 function draw() {
-  background(random(100, 150), random(0, 100), 0);
-  fill(mouseY, 150, 200);
+  background(10, random(100, 150), 255, 10);
+
+  strokeWeight(3);
+  noFill();
+  stroke(mouseY, 150, 200);
   circle(mouseX + 80, mouseY - 80, mouseX);
+
+  strokeWeight(1);
+  stroke(0, 255, 0);
   fill(0, 150, 200);
   rect(mouseX, mouseY, 100, 100);
 }
